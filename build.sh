075f3d160cef85a956d7683b0314ab5b2b2632b2
Michael Gugino <gugino.michael@yahoo.com>
2016-12-19 12:27:12 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+g++ -I /usr/include/kea -L /usr/lib/kea/lib -fpic -shared -o example.so \
+  load_unload.cc pkt4_receive.cc pkt4_send.cc version.cc \
+  -lkea-dhcpsrv -lkea-dhcp++ -lkea-hooks -lkea-log -lkea-util -lkea-exceptions -lzmq
